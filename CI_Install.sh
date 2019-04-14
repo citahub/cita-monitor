98f852c88fbffad0d05ceeb7a282fecc198e597f
jiangxianliang <jiangxianliang@cryptape.com>
2019-04-14 23:14:54 +0800

    Merge agent docker-compose and dockerfile directories, and partition port ranges

--- CI_Install.sh
+++ CI_Install.sh
@@ -6,7 +6,7 @@ NodeIP=`ifconfig eth0|grep "inet addr:"|awk -F":" '{print $2}'|awk '{print $1}'`
 OtherNode=1
 
 install_agent(){
-    cd ./cita-monitor-agent/docker-compose
+    cd ./cita-monitor-agent
     sed -i "s/1.1.1.1/${NodeIP}/g" .env
     sed -i "s/nodehostname/${Hostname}/g" .env
     docker-compose up -d
@@ -19,34 +19,34 @@ install_agent(){
         if  [[ ${OtherNode} -eq 1 ]]
         then
             echo "启动其他 node 监控"
-            docker run -d --name="prometheus_citaMonitorAgent_exporter__${NodeIP}_1338" \
+            docker run -d --name="prometheus_cita_exporter__${NodeIP}_1338" \
                 --pid="host" \
                 -p 1921:1920 \
                 -v "/data2/cita_secp256k1_sha3/test-chain/1":"/tmp/node" \
                 -v "/data2/cita_secp256k1_sha3/":"/tmp/softpath" \
                 -v "/data2":"/tmp/diskpath" \
-		-v "cita_agent_by_cita-cli.py":"/config" \
+		-v "cita_monitor_agent.py":"/config" \
                 -e Node="${NodeIP}:1338" \
                 -e NodeID=1 \
                 blankwu/cita_agent_by:cita-cli
-            docker run -d --name="prometheus_citaMonitorAgent_exporter__${NodeIP}_1339" \
+            docker run -d --name="prometheus_cita_exporter__${NodeIP}_1339" \
                 --pid="host" \
                 -p 1922:1920 \
                 -v "/data2/cita_secp256k1_sha3/test-chain/2":"/tmp/node" \
                 -v "/data2/cita_secp256k1_sha3/":"/tmp/softpath" \
                 -v "/data2":"/tmp/diskpath" \
-		-v "cita_agent_by_cita-cli.py":"/config" \
+		-v "cita_monitor_agent.py":"/config" \
                 -e Node="${NodeIP}:1339" \
                 -e NodeID=2 \
                 blankwu/cita_agent_by:cita-cli
 
-            docker run -d --name="prometheus_citaMonitorAgent_exporter__${NodeIP}_1340" \
+            docker run -d --name="prometheus_cita_exporter__${NodeIP}_1340" \
                 --pid="host" \
                 -p 1923:1920 \
                 -v "/data2/cita_secp256k1_sha3/test-chain/3":"/tmp/node" \
                 -v "/data2/cita_secp256k1_sha3/":"/tmp/softpath" \
                 -v "/data2":"/tmp/diskpath" \
-		-v "cita_agent_by_cita-cli.py":"/config" \
+		-v "cita_monitor_agent.py":"/config" \
                 -e Node="${NodeIP}:1340" \
                 -e NodeID=3 \
                 blankwu/cita_agent_by:cita-cli
